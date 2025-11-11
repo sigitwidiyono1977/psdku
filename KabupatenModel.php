d75f8436ecedce71e3906fc581947f1363d9d0f5
sigitwidiyono1977 <encriptman@gmail.com>
2025-11-11 15:39:55 +0700

    app/Models/KabupatenModel

---
+++ KabupatenModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Notifications\Notifiable;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KabupatenModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ref_kabupaten';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = ['kd_kabupaten', 'kabupaten', 'created_at', 'updated_at'];
+}
